ed0021d85d72a769b92d317b740ca17535ae19b2
小磊 <admin@jcwit.com>
2021-03-08 15:03:51 +0800

    获取自定义主机列表

--- d2-admin/src/views/batch/cmd/index.vue
+++ d2-admin/src/views/batch/cmd/index.vue
@@ -3,24 +3,20 @@
     <template slot="header">批量命令</template>
    <el-form :model="FormData" ref="cmdform" :rules="rules" label-width="100px" label-position='left'>
      <el-form-item label="主机组" prop="hostgroup">
-       <el-select v-model="FormData.hostgroup" placeholder="请选择主机组">
+       <el-select v-model="FormData.hostgroup" placeholder="请选择主机组" @change="changeHostgroup">
          <el-option v-for="item in HostGroupData" :key="item.value" :label="item.label" :value="item.value" ></el-option>
        </el-select>
-       <span class="error-text" v-if="grouperror">{{grouperror}}</span>
      </el-form-item>
      <el-form-item label="主机" prop="host">
         <el-select v-model="FormData.host" filterable multiple  placeholder="请选择主机">
           <el-option v-for="item in  HostData" :key="item.value" :label="item.label" :value="item.value">
           </el-option>
         </el-select>
-       <span class="error-text" v-if="hosterror">{{hosterror}}</span>
      </el-form-item>
      <el-form-item label="命令" prop="cmd">
        <el-input v-model="FormData.cmd"></el-input>
-       <span class="error-text" v-if="cmderror">{{cmderror}}</span>
      </el-form-item>
      <el-form-item>
-       <p class="error-text" v-show="error">{{error}}</p>
        <el-button type="primary" @click="onSubmit('cmdform')">执行命令</el-button>
      </el-form-item>
    </el-form>
@@ -31,7 +27,7 @@
 </template>
 
 <script>
-import {getselecthostgroup} from '@api/batch'
+import {getselecthostgroup,getgroup2host} from '@api/batch'
 export default {
 name: 'cmd',
 mounted () {
@@ -46,10 +42,6 @@ mounted () {
    },
    HostData:[],
    HostGroupData:[],
-   hosterror:false,
-   grouperror:false,
-   cmderror:false,
-   error:false,
    rules:{
      hostgroup:[
        {required:true,trigger:'blue',message:'请选择主机组'}
@@ -76,7 +68,16 @@ methods:{
     })
 
 
-  }
+  },
+  changeHostgroup(value){
+    getgroup2host({hostgroup:value}).then(res=>{
+        this.HostData=res;
+
+      }).catch(function (error){
+        console.log(error)
+      })
+
+  },
 }
 }
 </script>
